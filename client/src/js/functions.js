44dec38b6392fe0398618be3c3fef7dbeb28580c
kajdemuynck <kaj.de.muynck@student.howest.be>
2021-07-29 16:29:31 +0200

    Fixed memeber since not showing correctly

--- client/src/js/functions.js
+++ client/src/js/functions.js
@@ -6,7 +6,7 @@ export const formatDateFromMilliToDate = time => {
 };
 
 export const formatDateFromDateToNumber = time => {
-  const date = new Date(time);
+  const date = new Date(time * 1);
   const dateDate = `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
   return dateDate;
   //const dateTime = `${String(date.getHours()).padStart(2, '0')}${String(date.getMinutes()).padStart(2, '0')}${String(date.getSeconds()).padStart(2, '0')}${String(date.getMilliseconds()).padStart(3, '0')}`;
